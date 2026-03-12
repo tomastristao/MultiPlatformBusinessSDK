e568ba032b7f1b3b5d14ba91a6d7129154b742b8
toggletom <ttristao.contractor@libertyglobal.com>
2026-03-12 11:30:54 +0000

    update script

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
     id("com.android.library") version "8.5.2" apply false
     kotlin("android") version "2.0.21" apply false
-    id("maven-publish") apply false
 }
--- scripts/generate_sdks.rb
+++ scripts/generate_sdks.rb
@@ -1241,7 +1241,6 @@ def generate_android_root_build
     plugins {
         id("com.android.library") version "8.5.2" apply false
         kotlin("android") version "2.0.21" apply false
-        id("maven-publish") apply false
     }
   KOTLIN
 end
